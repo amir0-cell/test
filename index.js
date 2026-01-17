61c6421880fd8f2c770d58557c9aa67d7de4e79f
amir0-cell <amirsid950@gmail.com>
2026-01-18 04:05:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const a = 400;
-const a = 500;
-console.log(a);
+const password = "23tsagww";
+
+if(password == "1234abc"){
+  console.log("hello");
+}
+
+const b = 23;
+const b = 27;
+console.log(b);
